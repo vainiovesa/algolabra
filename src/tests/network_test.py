1e3103a5716614f3e9a6991edc2fd397a2ea2364
vainiovesa <vesa.vainio@helsinki.fi>
2025-07-19 11:33:45 +0300

    Add unit tests

--- src/tests/network_test.py
+++ src/tests/network_test.py
@@ -11,6 +11,13 @@ class TestNetwork(unittest.TestCase):
         self.inputs = np.array([1 for _ in range(n)])
         self.output = np.array([1 for _ in range(m)])
 
+        self.small_net = Network([2, 3, 1])
+        self.data = [(np.array([1, 1]), np.array([0])),
+                     (np.array([0, 1]), np.array([1])),
+                     (np.array([1, 0]), np.array([1])),
+                     (np.array([0, 0]), np.array([0]))]
+        self.lr = 3
+
     def test_weights_and_biases_right_type(self):
         for weight in self.net.weights:
             self.assertEqual(type(weight), np.ndarray)
@@ -50,16 +57,29 @@ class TestNetwork(unittest.TestCase):
             self.assertEqual(bd.shape, b.shape)
 
     def test_vanilla_gradient_descends(self):
-        net = Network([2, 3, 1])
-        data = [(np.array([1, 1]), np.array([0])),
-                (np.array([0, 1]), np.array([1])),
-                (np.array([1, 0]), np.array([1])),
-                (np.array([0, 0]), np.array([0]))]
         ep = 2000
-        lr = 3
-        learning_data = net.vanilla_gradient_descent(data, ep, lr)
+        learning_data = self.small_net.vanilla_gradient_descent(self.data, ep, self.lr)
         for i in range(1, ep):
             self.assertLess(learning_data[i], learning_data[i - 1])
-        for x, y in data:
-            rough_output = round(net.evaluate(x)[0])
+        for x, y in self.data:
+            rough_output = round(self.small_net.evaluate(x)[0])
+            self.assertEqual(rough_output, y[0])
+
+    def test_stochastic_gradient_descends(self):
+        ep = 1000
+        learning_data = self.small_net.stochastic_gradient_descent(self.data, ep, self.lr)
+        self.assertLess(learning_data[-1], learning_data[ep // 2])
+        self.assertLess(learning_data[ep // 2], learning_data[0])
+        for x, y in self.data:
+            rough_output = round(self.small_net.evaluate(x)[0])
+            self.assertEqual(rough_output, y[0])
+
+    def test_minibatch_gradient_descends(self):
+        ep = 1500
+        mb_size = 2
+        learning_data = self.small_net.minibatch_gradient_descent(self.data, mb_size, ep, self.lr)
+        self.assertLess(learning_data[-1], learning_data[ep // 2])
+        self.assertLess(learning_data[ep // 2], learning_data[0])
+        for x, y in self.data:
+            rough_output = round(self.small_net.evaluate(x)[0])
             self.assertEqual(rough_output, y[0])
